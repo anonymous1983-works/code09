46cd85d00d8388c3403e9471b1a167608a4329b7
Anis ABID <anisabid@MacBook-Pro-de-Anis.local>
2016-12-21 15:40:29 +0100

    save

--- app/scripts/components/popin.js
+++ app/scripts/components/popin.js
@@ -114,7 +114,7 @@
                 var test = new POPIN.popin.Popin({
                     "element" : document.querySelectorAll('.profile img')[0],
                     /*
-                    "otherClass" : ["textHome", "OtherClass2"],
+                    "otherClass" : ["OtherClass1", "OtherClass2"],
                     "title" : "tesazeazaaezzet",
                     "imgUrl" : "assets/icon-arrow.png",
                     "imgAlt" : "test",
